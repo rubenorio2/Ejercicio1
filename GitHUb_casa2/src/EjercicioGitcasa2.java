bd7a3c1df504b4a88d700e7d8fd32dd416b3e8e5
RUBEN <RUBEN@RUBEN-PC>
2018-05-18 12:48:04 +0200

    holis

--- GitHUb_casa2/src/EjercicioGitcasa2.java
+++ GitHUb_casa2/src/EjercicioGitcasa2.java
@@ -3,7 +3,7 @@ public class EjercicioGitcasa2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Esta echo por la mañana");
+			System.out.println("Hola");
 	}
 
 }
